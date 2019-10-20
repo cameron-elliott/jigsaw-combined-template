c5bed0f3eedd0987b153cdb1491c05a595e40679
cameron elliott <email@example.com>
2019-10-19 17:54:25 -0700

    add missing content width div and max-w-* classes

--- source/_layouts/category.blade.php
+++ source/_layouts/category.blade.php
@@ -8,19 +8,21 @@
 @endpush
 
 @section('body')
-    <h1>{{ $page->title }}</h1>
+    <div class="flex-auto w-full container max-w-4xl mx-auto py-16 px-6">   
+        <h1>{{ $page->title }}</h1>
 
-    <div class="text-2xl border-b border-blue-200 mb-6 pb-10">
-        @yield('content')
+        <div class="text-2xl border-b border-blue-200 mb-6 pb-10">
+            @yield('content')
+        </div>
+
+        @foreach ($page->posts($posts) as $post)
+            @include('_components.post-preview-inline')
+
+            @if (! $loop->last)
+                <hr class="w-full border-b mt-2 mb-6">
+            @endif
+        @endforeach
+
+        @include('_components.newsletter-signup')
     </div>
-
-    @foreach ($page->posts($posts) as $post)
-        @include('_components.post-preview-inline')
-
-        @if (! $loop->last)
-            <hr class="w-full border-b mt-2 mb-6">
-        @endif
-    @endforeach
-
-    @include('_components.newsletter-signup')
 @stop
